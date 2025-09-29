cc23f7610a9ca9cebd6c2a7e06de723859673873
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2025-09-29 14:29:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 let input1 = prompt("Enter the first number:");
 let input2 = prompt("Enter the second number:");
 
+// let sum = Number(input1) + Number(input2);
+let sum = parseInt(input1) + parseInt(input2);
+alert("The sum is : "+sum);
+
+
 // Write your code here and print the output using alert function
